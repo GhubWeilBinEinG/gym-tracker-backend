9d0d1006f929066e09c60df85c0bee6e4b316910
trangdo <trangdo2026@gmail.com>
2026-05-24 10:52:49 +0200

    Cross Origin added

--- src/main/java/htw_berlin/gym_tracker/GymTrackerController.java
+++ src/main/java/htw_berlin/gym_tracker/GymTrackerController.java
@@ -1,11 +1,14 @@
 package htw_berlin.gym_tracker;
 
 import org.apache.catalina.users.GenericRole;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:5173", "https://dein-frontend.onrender.com"
+        })
 @RestController
 public class GymTrackerController {
 
